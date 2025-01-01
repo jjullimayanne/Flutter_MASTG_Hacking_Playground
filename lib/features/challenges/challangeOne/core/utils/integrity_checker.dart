489a43544f4616eeed357107ff409397ac73c9ea
Julli Mayanne <jullimayanne9@gmail.com>
2025-01-01 15:08:56 -0300

    feat: add oneChallenge

--- lib/features/challenges/challangeOne/core/utils/integrity_checker.dart
+++ lib/features/challenges/challangeOne/core/utils/integrity_checker.dart
@@ -1,1 +1,14 @@
-//TODO DO needs implements 
+class IntegrityChecker {
+  static bool isDebuggerAttached() {
+    bool debug = false;
+    assert(debug = true);
+    return debug;
+  }
+
+  static void checkAppIntegrity() {
+    const String expectedHash = "abc123";
+    if ("abc123" != expectedHash) {
+      throw Exception("App integrity check failed!");
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_uncrackable/core/routes/route.dart';
 import 'package:flutter_uncrackable/core/routes/routes_constants.dart';
 
+import 'features/challenges/challangeOne/core/utils/integrity_checker.dart';
+
 void main() {
+  if (!kDebugMode) {
+    IntegrityChecker.checkAppIntegrity();
+    if (IntegrityChecker.isDebuggerAttached()) {
+      throw Exception("Debugger detected!");
+    }
+  }
+
   runApp(const MyApp());
 }
 
