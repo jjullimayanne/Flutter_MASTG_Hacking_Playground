506275c5781b87a46d106b54b9fef40acce7e0ad
Julli Mayanne <jullimayanne9@gmail.com>
2025-01-01 14:53:58 -0300

    feat: add oneChallenge

--- lib/features/challenges/challangeOne/core/utils/integrity_checker.dart
+++ lib/features/challenges/challangeOne/core/utils/integrity_checker.dart
@@ -1,14 +1,1 @@
-class IntegrityChecker {
-  static bool isDebuggerAttached() {
-    bool debug = false;
-    assert(debug = true);
-    return debug;
-  }
-
-  static void checkAppIntegrity() {
-    const String expectedHash = "abc123";
-    if ("abc123" != expectedHash) {
-      throw Exception("App integrity check failed!");
-    }
-  }
-}
+//TODO DO needs implements 
